3f4ddfd87e47c731f7394b6703d0107ca1cbe9f4
dev <magiur99@gmail.com>
2023-05-31 15:47:13 +0100

    add index.html

---
+++ build/precache-manifest.b8863380c6b1e4f098f8e625de3ec229.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0f6ab3efa230834e365",
+    "url": "/sanicodeplayground/solicitor-website/static/css/main.ab476971.chunk.css"
+  },
+  {
+    "revision": "c0f6ab3efa230834e365",
+    "url": "/sanicodeplayground/solicitor-website/static/js/main.ae7d369a.chunk.js"
+  },
+  {
+    "revision": "153cb5871a4fb993d436",
+    "url": "/sanicodeplayground/solicitor-website/static/js/runtime~main.ce513a6c.js"
+  },
+  {
+    "revision": "6285600ebc0511f31487",
+    "url": "/sanicodeplayground/solicitor-website/static/css/2.4baa4f4a.chunk.css"
+  },
+  {
+    "revision": "6285600ebc0511f31487",
+    "url": "/sanicodeplayground/solicitor-website/static/js/2.b2b4943c.chunk.js"
+  },
+  {
+    "revision": "abc0917fc73f305ffe194937e387accd",
+    "url": "/sanicodeplayground/solicitor-website/static/media/slide-1.abc0917f.jpg"
+  },
+  {
+    "revision": "cb75fcc8e23705a4eb8c51f352daa87e",
+    "url": "/sanicodeplayground/solicitor-website/static/media/slide-2.cb75fcc8.jpg"
+  },
+  {
+    "revision": "abc0917fc73f305ffe194937e387accd",
+    "url": "/sanicodeplayground/solicitor-website/static/media/slide-3.abc0917f.jpg"
+  },
+  {
+    "revision": "2e7a83f3ad21c50d5169610cdc98e7f6",
+    "url": "/sanicodeplayground/solicitor-website/static/media/image.2e7a83f3.webp"
+  },
+  {
+    "revision": "22354f56fb743461c13cfed09e0ebd49",
+    "url": "/sanicodeplayground/solicitor-website/static/media/CormorantGaramond-Regular.22354f56.ttf"
+  },
+  {
+    "revision": "6c964387bd5890b31ec84032876659b8",
+    "url": "/sanicodeplayground/solicitor-website/static/media/CormorantGaramond-Bold.6c964387.ttf"
+  },
+  {
+    "revision": "44f7560c3944822857673d252730cb8d",
+    "url": "/sanicodeplayground/solicitor-website/index.html"
+  }
+];
