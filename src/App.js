c8500c3336d967923cf63bbb23824762c4447b60
dev <magiur99@gmail.com>
2023-05-31 13:20:27 +0100

    create pages folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Home from './components/home/Home';
 import Header from './components/shared/Header';
+
+import AboutPage from './components/pages/AboutPage';
+import ContactPage from './components/pages/ContactPage';
+import HomePage from './components/pages/HomePage';
+import NewsPage from './components/pages/NewsPage';
+import ServicesPage from './components/pages/ServicesPage';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Home />
+      <HomePage />
+      <AboutPage />
+      <ContactPage />
+      <NewsPage />
+      <ServicesPage />
       {/* Add other components and routes as needed */}
     </div>
   );
