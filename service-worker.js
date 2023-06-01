1b5f7dd874877291630f33d6c904bf9559b7d25d
dev <magiur99@gmail.com>
2023-06-01 16:34:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/solicitor-website/precache-manifest.a2473e2211e94090d15c168b663423f9.js"
+  "/solicitor-website/precache-manifest.7a1012620aef310824d66d81c052cdf3.js"
 );
 
 workbox.clientsClaim();
