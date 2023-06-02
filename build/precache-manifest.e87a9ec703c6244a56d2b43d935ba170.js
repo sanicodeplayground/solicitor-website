0130f6f804dda90c42c267ea438f458c842fabc8
dev <magiur99@gmail.com>
2023-06-02 11:16:00 +0100

    build public url

---
+++ build/precache-manifest.e87a9ec703c6244a56d2b43d935ba170.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a4117030f1414ba240f",
+    "url": "/solicitor-website/static/css/main.34091f00.chunk.css"
+  },
+  {
+    "revision": "0a4117030f1414ba240f",
+    "url": "/solicitor-website/static/js/main.2e53b19e.chunk.js"
+  },
+  {
+    "revision": "df576336d4fde5fd88c8",
+    "url": "/solicitor-website/static/js/runtime~main.f1de3fc5.js"
+  },
+  {
+    "revision": "11076dde72e88f04dc26",
+    "url": "/solicitor-website/static/css/2.9fe6bf28.chunk.css"
+  },
+  {
+    "revision": "11076dde72e88f04dc26",
+    "url": "/solicitor-website/static/js/2.2dc5a98e.chunk.js"
+  },
+  {
+    "revision": "2e7a83f3ad21c50d5169610cdc98e7f6",
+    "url": "/solicitor-website/static/media/image.2e7a83f3.webp"
+  },
+  {
+    "revision": "1e2fc5d0f8cdc377c8be2c673560c96b",
+    "url": "/solicitor-website/static/media/profile-1.1e2fc5d0.jpg"
+  },
+  {
+    "revision": "002209855485ee3f74482e6c74688593",
+    "url": "/solicitor-website/static/media/profile-2.00220985.jpg"
+  },
+  {
+    "revision": "86d6cce9332ed6f4058940a8e37613f3",
+    "url": "/solicitor-website/static/media/profile-3.86d6cce9.jpg"
+  },
+  {
+    "revision": "cc59b342f26bb22c3ed62b5b594c45fc",
+    "url": "/solicitor-website/static/media/profile-4.cc59b342.jpg"
+  },
+  {
+    "revision": "7804e62ea9fcfbcbeb7be7e8a8056295",
+    "url": "/solicitor-website/static/media/profile-5.7804e62e.jpg"
+  },
+  {
+    "revision": "7e41f6d8c599919a1f88eb47b918d227",
+    "url": "/solicitor-website/static/media/profile-6.7e41f6d8.jpg"
+  },
+  {
+    "revision": "a2e05008c25713149834da9f3a25def4",
+    "url": "/solicitor-website/static/media/profile-7.a2e05008.jpg"
+  },
+  {
+    "revision": "e1bf8a4e3390ed89a6f1744b2ec5967a",
+    "url": "/solicitor-website/static/media/profile-8.e1bf8a4e.jpg"
+  },
+  {
+    "revision": "dc59e48f24d009b209ed46e35e3d1733",
+    "url": "/solicitor-website/static/media/profile-9.dc59e48f.jpg"
+  },
+  {
+    "revision": "ab15749c54e64577d0ded3c7c32159e4",
+    "url": "/solicitor-website/static/media/slide-1.ab15749c.jpg"
+  },
+  {
+    "revision": "e43b38bcf16b18bb1af7e05fc23b8b99",
+    "url": "/solicitor-website/static/media/slide-2.e43b38bc.jpg"
+  },
+  {
+    "revision": "27c3af15104913a61e6ae9e20c44d2fd",
+    "url": "/solicitor-website/static/media/slide-3.27c3af15.jpg"
+  },
+  {
+    "revision": "5f872bdeb4e2c44cb7297246adba6693",
+    "url": "/solicitor-website/static/media/features-1.5f872bde.jpg"
+  },
+  {
+    "revision": "a5c9f3c9d352d539e5531b505bc81c20",
+    "url": "/solicitor-website/static/media/features-2.a5c9f3c9.jpg"
+  },
+  {
+    "revision": "42158d9c753fa36fe3fc0a5bebeae955",
+    "url": "/solicitor-website/static/media/features-3.42158d9c.jpg"
+  },
+  {
+    "revision": "0a9e92ef0cd65db6fc973347db820e0e",
+    "url": "/solicitor-website/static/media/logo.0a9e92ef.png"
+  },
+  {
+    "revision": "a30fb81bd52143bcd4de2898422ac8b9",
+    "url": "/solicitor-website/static/media/bootstrap-icons.a30fb81b.woff2"
+  },
+  {
+    "revision": "3d0ec57f40d62ba6a8e0d1857bb8f7d5",
+    "url": "/solicitor-website/static/media/bootstrap-icons.3d0ec57f.woff"
+  },
+  {
+    "revision": "22354f56fb743461c13cfed09e0ebd49",
+    "url": "/solicitor-website/static/media/CormorantGaramond-Regular.22354f56.ttf"
+  },
+  {
+    "revision": "6c964387bd5890b31ec84032876659b8",
+    "url": "/solicitor-website/static/media/CormorantGaramond-Bold.6c964387.ttf"
+  },
+  {
+    "revision": "9fd43f7c3541c12e1c6d1ff6d8c5350b",
+    "url": "/solicitor-website/index.html"
+  }
+];
